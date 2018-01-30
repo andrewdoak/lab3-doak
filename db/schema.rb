e205b46b772378787221ad44906daa1e2d269f86
Andrew <thedoak@gmail.com>
2018-01-30 00:29:08 +0000

    added quotes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110172117) do
+ActiveRecord::Schema.define(version: 20180129221105) do
+
+  create_table "quotes", force: :cascade do |t|
+    t.string "author"
+    t.string "quotation"
+    t.string "source"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
